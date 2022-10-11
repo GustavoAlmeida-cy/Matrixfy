a5675ded48498ac1175d3594a8e5710ec813447c
Gustavo <gustavo.almeida.cy@gmail.com>
2022-10-11 15:02:56 -0300

    colors and drag 'n' drop

--- js/sketch.js
+++ js/sketch.js
@@ -1,15 +1,40 @@
-// Charset variables
-const charSet = "Ñ@#W$9876543210?!abc;:+=-,._                ";
-// const charSet = ".:-i|=+%0#@";
+const density = "Ñ@#W$9876543210?!abc;:+=-,._              ";
+// const density = " .:-i|=+%0#@";
 
 let img;
+let dropzone;
+let body;
 
 function preload() {
-  img = loadImage("../images/arch.png");
+  img = loadImage("../neo.jpg");
 }
 
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(600, 600);
+  img.resize(50, 50);
+  dropzone = select("main");
+  body = select("body");
+}
+
+function draw() {
+  clear();
+
+  dropzone.dragOver(highlight);
+  dropzone.dragLeave(unHighlight);
+  dropzone.drop(getFile, unHighlight);
+
+  function highlight() {
+    dropzone.style("background-color", "rgba(255,255,255,0.1");
+  }
+
+  function unHighlight() {
+    dropzone.style("background-color", "rgba(0,0,0,0");
+  }
+
+  function getFile(file) {
+    img = loadImage(file.data);
+  }
+
   img.resize(50, 50);
 
   let w = width / img.width;
@@ -17,31 +42,23 @@ function setup() {
 
   img.loadPixels();
 
-  // Main feature
-  for (let i = 0; i < img.height; i++) {
-    // Column
-    for (let j = 0; j < img.width; j++) {
-      // Row
-      const pixelIndex = (i + j * img.width) * 4; // Get pixels index
-
-      // Get pixels colors
+  for (let j = 0; j < img.height; j++) {
+    for (let i = 0; i < img.width; i++) {
+      const pixelIndex = (i + j * img.width) * 4;
       const r = img.pixels[pixelIndex + 0];
       const g = img.pixels[pixelIndex + 1];
       const b = img.pixels[pixelIndex + 2];
 
-      // Gray scale
       const avg = (r + g + b) / 3;
 
-      fill(225);
-
-      // Pixels to Chars
-      const len = charSet.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-
-      // Text settings
+      fill(r, g, b);
       textSize(w);
       textAlign(CENTER, CENTER);
-      text(charSet.charAt(charIndex), i * h + h * 0.5, j * w + w * 0.5);
+
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, 0, len));
+
+      text(density.charAt(charIndex), i * w + w * 0.5, j * h + h * 0.5);
     }
   }
 }
